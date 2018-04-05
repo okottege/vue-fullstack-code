c99907e04094911f5a4cf3ec0e2a676c3045d38c
Oshan Kottege <oshan.kottege@gmail.com>
2018-04-05 22:35:50 +1200

    Got the notes adding functionality going.

---
+++ notes-app/src/app/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="mainContainer">
+    <div class="notes-section">
+      <div class="columns">
+        <div class="column has-text-centered">
+          <strong>Notes</strong>
+          <div v-for="note in notes" class="notes" v-bind:key="note">
+            {{ note }}
+          </div>
+        </div>
+        <div class="column has-text-centered">
+          <strong>Timestamp</strong>
+          <div v-for="timestamp in timestamps" class="timestamps" v-bind:key="timestamp">
+            {{ timestamp }}
+          </div>
+        </div>
+      </div>
+      <input-component @add-note="addNote" :placeholder="placeholder"></input-component>
+    </div>
+  </div>
+</template>
+
+<script>
+import InputComponent from './components/InputComponent';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      notes: [],
+      timestamps: [],
+      placeholder: 'Enter a Note'
+    };
+  },
+  methods: {
+    addNote(event) {
+      this.notes.push(event.note);
+      this.timestamps.push(event.timestamp)
+    }
+  },
+  components: {
+    InputComponent
+  }
+}
+</script>
---
+++ notes-app/src/app/components/InputComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <input
+      :placeholder="placeholder"
+      class="input is-small"
+      type="text"
+      v-model="input"
+      @keyup.enter="monitorEnterKey" />
+</template>
+
+<script>
+export default {
+  name: 'InputComponent',
+  props: ['placeholder'],
+  data() {
+    return {
+      input: ''
+    };
+  },
+  methods: {
+    monitorEnterKey() {
+      this.$emit('add-note', {
+        note: this.input,
+        timestamp: new Date().toLocaleString()
+      });
+      this.input = '';
+    }
+  }
+}
+</script>
+
