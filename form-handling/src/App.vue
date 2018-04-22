31c243578493e6bae69bd00b08efca3e804ced07
Oshan Kottege <oshan.kottege@gmail.com>
2018-04-22 22:09:05 +1200

    Initial work for forms

---
+++ form-handling/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div class="ui-container">
+      <h2 class="ui header">What's your favourite Fullstack clothing item?</h2>
+      <button-row></button-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import ButtonRow from './components/ButtonRow';
+
+export default {
+  name: 'App',
+  components: {
+    ButtonRow,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ form-handling/src/components/ButtonRow.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <button @click="onHoodieClick" class="ui button">Hoodie</button>
+    <button @click="onTeeClick" class="ui button">Tee</button>
+    <button @click="onFittedCapClick" class="ui button">Fitted Cap</button>
+    <button @click="onJacketClick" class="ui button">Jacket</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonRow',
+  methods: {
+    onHoodieClick(e) {
+      console.log('The user clicked button-hoodie', e);
+    },
+    onTeeClick(e) {
+      console.log('The user clicked button-tee', e);
+    },
+    onFittedCapClick(e) {
+      console.log('The user clicked button-fitted-cap', e);
+    },
+    onJacketClick(e) {
+      console.log('The user clicked button-jacked', e);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
