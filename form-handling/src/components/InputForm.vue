9b368f2f45970a9ab789d032b85fa836d092458a
Oshan Kottege <oshan.kottege@gmail.com>
2018-04-24 16:16:51 +1200

    Added a form with multiple fields.

--- form-handling/src/components/InputForm.vue
+++ form-handling/src/components/InputForm.vue
@@ -2,7 +2,27 @@
   <div class="input-form">
     <form @submit="submitForm" class="ui form">
       <div class="field">
-        <input v-model="newItem" type="text" placeholder="Add an item!" />
+        <label>New Item</label>
+        <input v-model="fields.newItem" type="text" placeholder="Add an item!" />
+      </div>
+      <div class="field">
+        <label>Email</label>
+        <input v-model="fields.email" type="text" placeholder="What's your email?" />
+      </div>
+      <div class="field">
+        <label>Urgency</label>
+        <select v-model="fields.urgency" class="ui fluid search dropdown">
+          <option disabled value="">Please select one</option>
+          <option>Nonessential</option>
+          <option>Moderate</option>
+          <option>Urgent</option>
+        </select>
+      </div>
+      <div class="field">
+        <div class="ui checkbox">
+          <input v-model="fields.termsAndConditions" type="checkbox" />
+          <label>I accept the Terms and Conditions</label>
+        </div>
       </div>
       <button class="ui button">Submit</button>
     </form>
@@ -16,10 +36,15 @@
 </template>
 <script>
 export default {
-  name: "InputForm",
+  name: 'InputForm',
   data() {
     return {
-      newItem: '',
+      fields: {
+        newItem: '',
+        email: '',
+        urgency: '',
+        termsAndConditions: false,
+      },
       items: [],
     };
   },
@@ -29,6 +54,6 @@ export default {
       this.newItem = '';
       e.preventDefault();
     },
-  }
-}
+  },
+};
 </script>
