ccc36374daf3ed80af4d75f6c5badc830052fb25
Oshan Kottege <oshan.kottege@gmail.com>
2018-05-01 21:27:04 +1200

    Started working on the vue routing.

---
+++ routing/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div class="movies">
+      <h2>Which movie?</h2>
+      <a href="/dunkirk">/dunkirk</a>
+      <a href="/interstellar">/interstellar</a>
+      <a href="/the-dark-knight-rises">/the-dark-knight-rises</a>
+
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  comments: {},
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
