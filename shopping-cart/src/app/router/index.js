870e2d2b9ae61c8e4d41eaeb9244a4e958adb6e1
Oshan Kottege <oshan.kottege@gmail.com>
2018-05-07 21:50:23 +1200

    Added the product details page and its route.

--- shopping-cart/src/app/router/index.js
+++ shopping-cart/src/app/router/index.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router';
 import CartList from '../components/cart/CartList';
 import ProductList from '../components/product/ProductList';
 import NotFound from '../components/NotFound';
+import ProductItem from '../components/product/ProductItem';
 
 Vue.use(VueRouter);
 
@@ -14,6 +15,7 @@ const router = new VueRouter({
         { path: '/cart', component: CartList },
         { path: '/', redirect: '/products' },
         { path: '*', component: NotFound },
+        { path: '/products/:id', component: ProductItem, props: true },
     ],
 });
 
