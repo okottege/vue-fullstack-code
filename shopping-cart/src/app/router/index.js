95bf6a75a1380aa274110508db12f15ba412b324
Oshan Kottege <oshan.kottege@myob.com>
2018-05-08 08:54:24 +1200

    Started implementing the login functionality.

--- shopping-cart/src/app/router/index.js
+++ shopping-cart/src/app/router/index.js
@@ -5,6 +5,7 @@ import CartList from '../components/cart/CartList';
 import ProductList from '../components/product/ProductList';
 import NotFound from '../components/NotFound';
 import ProductItem from '../components/product/ProductItem';
+import LoginBox from '../components/login/LoginBox';
 
 Vue.use(VueRouter);
 
@@ -16,6 +17,7 @@ const router = new VueRouter({
         { path: '/', redirect: '/products' },
         { path: '*', component: NotFound },
         { path: '/products/:id', component: ProductItem, props: true },
+        { path: '/login', component: LoginBox },
     ],
 });
 
