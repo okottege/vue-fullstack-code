e0b7b381229d632ebccaa876b06b577ee60d41ca
Oshan Kottege <oshan.kottege@myob.com>
2018-05-11 16:34:28 +1200

    Completed the basic level unit tests.

--- testing/basic/test/specs/App.spec.js
+++ testing/basic/test/specs/App.spec.js
@@ -61,12 +61,52 @@ describe('App.vue', () => {
 
     describe('and then submits the form', () => {
       let addItemButton;
+      let itemList;
+      let inputField;
 
       beforeEach(() => {
-        wrapper.setData({item: 'New Item'});
         addItemButton = wrapper.find('.ui.button');
+        itemList = wrapper.find(' .item-list');
+        inputField = wrapper.find('input');
+
+        wrapper.setData({item: 'New Item'});
         addItemButton.trigger('click');
       });
+
+      it('should add a new item to the "items" data property', () => {
+        expect(wrapper.vm.items).to.contain('New Item');
+        expect(itemList.html()).to.contain('<td>New Item</td>');
+      });
+
+      it('should set the "item" data property to a blank string', () => {
+        expect(wrapper.vm.item).to.equal('');
+        expect(inputField.element.value).to.equal('');
+      });
+
+      it('should disable the "Add" button', () => {
+        expect(addItemButton.element.disabled).to.be.true;
+      });
     });
+  });
+
+  describe('the user clics the "Remove all" label', () => {
+    let itemList;
+    let removeItemsLabel;
+
+    beforeEach(() => {
+      itemList = wrapper.find(' .item-list');
+      removeItemsLabel = wrapper.find(' .ui.label');
+
+      wrapper.setData({items: ['Item #1', 'Item #2', 'Item #3']});
+    });
+
+    it('should remove all items from the "items" data property.', () => {
+      removeItemsLabel.trigger('click');
+
+      expect(wrapper.vm.items).to.deep.equal([]);
+      expect(itemList.html()).to.not.contain('<td>Item #1</td>');
+      expect(itemList.html()).to.not.contain('<td>Item #2</td>');
+      expect(itemList.html()).to.not.contain('<td>Item #3</td>');
+    })
   });
 });
