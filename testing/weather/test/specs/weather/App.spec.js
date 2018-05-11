95d5e7d9e50e50bd4a67a758b706b5164ed1033e
Oshan Kottege <oshan.kottege@myob.com>
2018-05-11 17:40:44 +1200

    Added a test for current date display.

--- testing/weather/test/specs/weather/App.spec.js
+++ testing/weather/test/specs/weather/App.spec.js
@@ -1,5 +1,28 @@
 import App from '@/App';
+import { shallow, createLocalVue } from 'vue-test-utils';
+import Vuex from 'vuex';
 
 describe('App.vue', () => {
+    let wrapper;
+    let store;
+    let getters;
 
+    beforeEach(() => {
+        const localVue = createLocalVue();
+        localVue.use(Vuex);
+
+        getters = {
+            loading: () => false,
+        };
+        store = new Vuex.Store({
+            getters
+        });
+
+        wrapper = shallow(App, { localVue, store });
+    });
+
+    it("should display the current day's date", () => {
+        const formattedDate = new Date().toDateString();
+        expect(wrapper.html()).to.contain(formattedDate);
+    });
 });
