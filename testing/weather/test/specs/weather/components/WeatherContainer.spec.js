72c359fe6ae18f3a3375c94efcca2904bc28b55d
Oshan Kottege <oshan.kottege@gmail.com>
2018-05-14 08:54:56 +1200

    Started working on weather container tests.

--- testing/weather/test/specs/weather/components/WeatherContainer.spec.js
+++ testing/weather/test/specs/weather/components/WeatherContainer.spec.js
@@ -1,5 +1,46 @@
 import WeatherContainer from '@/components/WeatherContainer';
+import { shallow, createLocalVue } from 'vue-test-utils';
+import Vuex from 'vuex';
+import sinon from 'sinon';
 
 describe('WeatherContainer.vue', () => {
-  
+  let wrapper;
+  let getters;
+  let actions;
+  let store;
+  let localVue;
+
+  const setupWrapper = (loading) => {
+    localVue = createLocalVue();
+    localVue.use(Vuex);
+
+    getters = {
+      location: () => 'New York',
+      weatherDescription: () => 'Light Cloud',
+      imageAbbr: () => 'lc.png',
+      weatherTemp: () => -10.0,
+      loading: () => loading,
+    };
+
+    actions = {
+      fetchWeather: sinon.stub(),
+    };
+
+    store = new Vuex.Store ({
+      getters,
+      actions,
+    });
+
+    wrapper = shallow(WeatherContainer, { localVue, store });
+  };
+
+  it('should render the correct content when the app is loading', () => {
+    setupWrapper(true);
+
+    
+  });
+
+  it('should render the correct content when the app is not loading', () => {
+
+  });
 });
